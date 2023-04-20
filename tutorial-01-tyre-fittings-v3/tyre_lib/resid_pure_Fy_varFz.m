0e16b1a9faafdf2c0648dce3f31f07f207e41a26
GiorgioZamp <giorgio.zampieri@studenti.unitn.it>
2023-04-20 22:15:43 +0200

    Reviewed Long

--- tutorial-01-tyre-fittings-v3/tyre_lib/resid_pure_Fy_varFz.m
+++ tutorial-01-tyre-fittings-v3/tyre_lib/resid_pure_Fy_varFz.m
@@ -11,14 +11,12 @@ function res = resid_pure_Fy_varFz(P,FY,ALPHA,GAMMA,FZ,tyre_data)
     
     tmp_tyre_data = tyre_data;
     
-    % Biral Given (reasonable)
+   
     tmp_tyre_data.pDy2 = P(1); 
     tmp_tyre_data.pEy2 = P(2);
     tmp_tyre_data.pHy2 = P(3);
     tmp_tyre_data.pVy2 = P(4);
     
-
-
 
     % Longitudinal Force (Pure Longitudinal Slip) Equations
     res = 0;
